088f825a25731442eed2a20ddaa1052849b4c116
[Sumit] <[sumitadhikari166@gmail.com]>
2024-06-27 17:35:36 +0530

    frontend registration,login,change-password,send email add

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -5,7 +5,7 @@ const Home = () => {
     <>
       <Navbar />
       <h2 className="text-3xl font-bold text-center p-5">Welcome to Passport JS + JWT Authentication</h2>
-      <h3 className="text-2xl font-bold text-center p-5">yyou can access token and refresh token on the server side</h3>
+      <h3 className="text-2xl font-bold text-center p-5">you can access token and refresh token on the server side</h3>
     </>
   )
 }
